4fc305619e32c2e5fec8f4c8ae58c7a6a0e1d432
wwIrvingww <mor22781@guvg.edu.gt>
2023-09-20 10:42:59 -0600

    Prueba de commit a branch.

--- app/src/main/java/com/example/secondtry/MainActivity.kt
+++ app/src/main/java/com/example/secondtry/MainActivity.kt
@@ -50,7 +50,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SecondTryTheme {
-                // A surface container using the 'background' color from the theme
+                // A surface container using the 'background' color from the theme, si
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
